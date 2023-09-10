35076d7b4a44e03a9acd91b55009cebe35e6466c
mplac555 <marcelo.placencia@epn.edu.ec>
2023-09-09 20:06:17 -0500

    BrigadeMembers API implemented

--- client/src/components/pages/BrigadeMembers.jsx
+++ client/src/components/pages/BrigadeMembers.jsx
@@ -1,28 +1,27 @@
 // IMPORTS: dependencies
-import React from 'react';
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Route, Routes } from "react-router-dom";
-// IMPORTS: components
-import ListManager from "../../modules/ListManager";
+import axios from "axios";
+// IMPORTS: custom components
 import ElementListPage from "../other/ElementListPage";
-import ElementMgmentPage from "../other/ElementMgmentPage";
-import BrigadistaForm from '../formularios/BrigadistaForm';
-import axios from 'axios';
+import BrigadistaForm from "../formularios/BrigadistaForm";
+// import ElementMgmentPage from "../other/ElementMgmentPage";
 // IMPORTS: data
+import ListManager from "../../modules/ListManager";
 import {
-  initialBrigMembersList,
+  // initialBrigMembersList,
   brigMemberFields,
 } from "../../data/BrigMembersData";
 
-const BASE_API_URL = 'http://localhost:8000/api';
+const BASE_API_URL = "http://localhost:8000/api";
 
 // MAIN COMPONENT
 export default function BrigMembRoutes() {
-  const [list_, _] = useState(initialBrigMembersList);
-  const [list, setBrigadistasList] = React.useState([]);
+  const [list, setBrigadistasList] = useState([]);
+  // const [list_, _] = useState(initialBrigMembersList);
 
   // Cargar la lista de autores al montar el componente
-  React.useEffect(() => {
+  useEffect(() => {
     fetchBrigadistas();
   }, []);
 
@@ -31,25 +30,29 @@ export default function BrigMembRoutes() {
     try {
       const response = await axios.get(`${BASE_API_URL}/brigadistas`);
       setBrigadistasList(response.data);
-      console.log(response.data);
-      console.log('Atencion...');
-      console.log(brigMemberFields);
+      // console.log(response.data);
+      // console.log('Atencion...');
+      // console.log(brigMemberFields);
     } catch (error) {
-      console.error('Error fetching admins:', error);
+      console.error("Error fetching admins:", error);
     }
   };
 
+  function deleteHandler(list, ids) {
+    ListManager.deleteElement(list, ids, `${BASE_API_URL}/brigadista`);
+  }
+
   // Functions to handle adding, editing and searching for brigade
   // members to/from the list
-  function addMember(newMember) {
-    ListManager.add(list, newMember);
-  }
-  function editMember(member) {
-    ListManager.editElement(list, member);
-  }
-  function searchMember(id) {
-    return ListManager.searchElementById(list, id);
-  }
+  // function addMember(newMember) {
+  //   ListManager.add(list, newMember);
+  // }
+  // function editMember(member) {
+  //   ListManager.editElement(list, member);
+  // }
+  // function searchMember(id) {
+  //   return ListManager.searchElementById(list, id);
+  // }
 
   return (
     <Routes>
@@ -61,18 +64,15 @@ export default function BrigMembRoutes() {
             list={list}
             fields={brigMemberFields}
             name="Brigadista"
+            deleteHandler={deleteHandler}
           />
         }
       />
       {/*CREATE NEW BRIGADE MEMBER ROUTE*/}
-      <Route
-        path="agregar"
-        element={
-          <BrigadistaForm/>
-        }
-      />
+      <Route path="agregar" element={<BrigadistaForm list={list} />} />
       {/*EDIT BRIGADE MEMBER ROUTE*/}
-      <Route
+      <Route path="editar/:id" element={<BrigadistaForm list={list} />} />
+      {/* <Route
         path="editar/:id"
         element={
           <ElementMgmentPage
@@ -83,7 +83,7 @@ export default function BrigMembRoutes() {
             classN="brigs"
           />
         }
-      />
+      /> */}
     </Routes>
   );
 }
--- server/controllers/brigadista.controller.js
+++ server/controllers/brigadista.controller.js
@@ -1,40 +1,57 @@
 // Importamos el modelo 'Admin' que contiene el esquema y la funcionalidad para interactuar con la colección de administradores en la base de datos.
-const Brigadista = require('../models/brigadista.model.js');
+const Brigadista = require("../models/brigadista.model.js");
 
 // Controlador para crear un nuevo administrador en la base de datos.
 module.exports.createBrigadista = (request, response) => {
-    console.log(request.body); // Imprimimos en la consola el cuerpo de la solicitud recibida (datos del nuevo administrador).
-    const { brigName, brigLastname, brigDNI, brigMail, brigPassword, brigPhone, brigAddress } = request.body; // Extraemos el adminName, adminLastname, adminDNI, adminMail, adminPassword y adminPhone del cuerpo de la solicitud.
-    Brigadista.create({
-        brigName,
-        brigLastname,
-        brigDNI,
-        brigMail,
-        brigPassword,
-        brigPhone,
-        brigAddress
-    })
-    .then(brigadista => response.json(brigadista)) // Si se crea correctamente, devolvemos el nuevo administrador en formato JSON como respuesta.
-    .catch(err => response.status(400).json(err)); // Si ocurre un error, devolvemos el error en formato JSON con un código de estado 400 (solicitud incorrecta).
-}
+  console.log(request.body); // Imprimimos en la consola el cuerpo de la solicitud recibida (datos del nuevo administrador).
+  const {
+    brigName,
+    brigLastname,
+    brigDNI,
+    brigMail,
+    brigPassword,
+    brigPhone,
+    brigAddress,
+  } = request.body; // Extraemos el adminName, adminLastname, adminDNI, adminMail, adminPassword y adminPhone del cuerpo de la solicitud.
+  Brigadista.create({
+    brigName,
+    brigLastname,
+    brigDNI,
+    brigMail,
+    brigPassword,
+    brigPhone,
+    brigAddress,
+  })
+    .then((brigadista) => response.json(brigadista)) // Si se crea correctamente, devolvemos el nuevo administrador en formato JSON como respuesta.
+    .catch((err) => response.status(400).json(err)); // Si ocurre un error, devolvemos el error en formato JSON con un código de estado 400 (solicitud incorrecta).
+};
 
 // Controlador para obtener todos los administradors de la base de datos, ordenados por edad de forma ascendente.
 module.exports.getAllBrigadistas = (_, response) => {
-    Brigadista.find({}).sort({ brigName: 1, brigLastname: 1 }) // Ordenamos por 'adminName' y 'adminLastname' en orden alfabético ascendente (1).
-    .then(brigadista => response.json(brigadista)) // Devolvemos todos los administradores encontrados en formato JSON como respuesta.
-    .catch(err => response.json(err)); // Si ocurre un error, devolvemos el error en formato JSON.
-}
+  Brigadista.find({})
+    .sort({ brigName: 1, brigLastname: 1 }) // Ordenamos por 'adminName' y 'adminLastname' en orden alfabético ascendente (1).
+    .then((brigadista) => response.json(brigadista)) // Devolvemos todos los administradores encontrados en formato JSON como respuesta.
+    .catch((err) => response.json(err)); // Si ocurre un error, devolvemos el error en formato JSON.
+};
 
 // Controlador para obtener un administrador específico de la base de datos según su ID.
 module.exports.getBrigadista = (request, response) => {
-    Brigadista.findOne({ _id: request.params.id }) // Buscamos un administrador por su ID proporcionado en los parámetros de la URL.
-    .then(brigadista => response.json(brigadista)) // Devolvemos el administrador encontrado en formato JSON como respuesta.
-    .catch(err => response.json(err)); // Si ocurre un error, devolvemos el error en formato JSON.
-}
+  Brigadista.findOne({ _id: request.params.id }) // Buscamos un administrador por su ID proporcionado en los parámetros de la URL.
+    .then((brigadista) => response.json(brigadista)) // Devolvemos el administrador encontrado en formato JSON como respuesta.
+    .catch((err) => response.json(err)); // Si ocurre un error, devolvemos el error en formato JSON.
+};
 
 // Controlador para eliminar un administrador de la base de datos según su ID.
 module.exports.deleteBrigadista = (request, response) => {
-    Brigadista.deleteOne({ _id: request.params.id }) // Buscamos y eliminamos el administrador por su ID proporcionado en los parámetros de la URL.
-    .then(brigadista => response.json(brigadista)) // Devolvemos el resultado de la eliminación en formato JSON como respuesta.
-    .catch(err => response.json(err)); // Si ocurre un error, devolvemos el error en formato JSON.
-}
+  Brigadista.deleteOne({ _id: request.params.id }) // Buscamos y eliminamos el administrador por su ID proporcionado en los parámetros de la URL.
+    .then((brigadista) => response.json(brigadista)) // Devolvemos el resultado de la eliminación en formato JSON como respuesta.
+    .catch((err) => response.json(err)); // Si ocurre un error, devolvemos el error en formato JSON.
+};
+
+module.exports.updateBrigadista = (request, response) => {
+  Brigadista.findOneAndUpdate({ _id: request.body._id }, request.body, {
+    new: true,
+  })
+    .then((brigadista) => response.json(brigadista))
+    .catch((err) => response.json(err));
+};
