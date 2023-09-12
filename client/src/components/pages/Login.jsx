b4ff11387dd6b5892f6e2dbbf008233532b00c7a
mplac555 <marcelo.placencia@epn.edu.ec>
2023-09-11 20:16:40 -0500

    Auth logic implemented in web client

--- client/src/components/pages/Login.jsx
+++ client/src/components/pages/Login.jsx
@@ -1,8 +1,9 @@
 // IMPORTS: dependencies
-import {createElement, useState} from "react";
+import { createElement, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import "../../styles/Login_Style.css";
-//IMPORTS: components 
+import axios from "axios";
+//IMPORTS: components
 import {
   Box,
   Button,
@@ -26,8 +27,12 @@ import {
 import LoginMethods from "../../modules/LoginMethods";
 // IMPORTS: data
 import {
-  initialBrigMembersList,brigMemberFields,} from "../../data/BrigMembersData";
+  initialBrigMembersList,
+  brigMemberFields,
+} from "../../data/BrigMembersData";
 import { initialAdminsList } from "../../data/AdminsData";
+
+const BASE_API_URL = "http://localhost:8000/api";
 
 //styles
 const estilo = {
@@ -43,19 +48,18 @@ const iconos = {
   password: <Lock sx={estilo} />,
 };
 const campos = [
-  {id: "nombre", label: "Nombre de Usuario"},
-  {id: "correo", label: "Correo electrónico"},
-  {id: "password", label: "Contraseña"},
+  { id: "nombre", label: "Nombre de Usuario" },
+  { id: "correo", label: "Correo electrónico" },
+  { id: "password", label: "Contraseña" },
 ];
 
 function Login() {
-
   //State
   const [rol, definirRol] = useState("");
   const [userName, setuserName] = useState("");
-  const [email,setEmail] = useState("");
-  const [pass,setpassword] = useState("");
-  const [eLogin, setELogin] = useState("");
+  const [email, setEmail] = useState("");
+  const [pass, setpassword] = useState("");
+  const [eLogin, setELogin] = useState(false);
   //navigate
   let navigate = useNavigate();
   //Brigade list
@@ -64,52 +68,173 @@ function Login() {
   const [listA] = useState(initialAdminsList);
 
   //Metodo que permite verificar el rol (Admin o brigadista y verifica que nombre, correo y contraseña coincida)
-  const startSession = () =>{
+  const startSession = () => {
     //Admin login
-    if(rol == "Administrador"){
-
+    if (rol == "Administrador") {
       console.log("Eres un admin");
-      const atributosA = {vname: userName, vemail: email, vpassword: pass};
-      const encontradoA = LoginMethods.loginBrigade(listA,atributosA);
+      const atributosA = { vname: userName, vemail: email, vpassword: pass };
+      const encontradoA = LoginMethods.loginBrigade(listA, atributosA);
       console.log(encontradoA);
 
-      if(encontradoA != null){
-
+      if (encontradoA != null) {
         console.log("Exitoso");
         console.log(encontradoA);
         navigate("/");
-      }else{
+      } else {
         console.log("Error");
-        setELogin("El Correo, el Nombre del Usuario o la Contraseña son Incorrectos. Por favor Intentelo de nuevo.")
+        setELogin(
+          "El Correo, el Nombre del Usuario o la Contraseña son Incorrectos. Por favor Intentelo de nuevo."
+        );
       }
 
-    //Brigade login
-    }else if(rol == "Brigadista"){
+      //BrigadeMember login
+    } else if (rol == "Brigadista") {
       console.log("Eres un brigadista");
-      const atributos = {vname: userName, vemail: email, vpassword: pass};
-      const encontrado = LoginMethods.loginBrigade(list,atributos);
+      const atributos = { vname: userName, vemail: email, vpassword: pass };
+      const encontrado = LoginMethods.loginBrigade(list, atributos);
 
-      if(encontrado != null){
-
+      if (encontrado != null) {
         console.log("Exitoso");
         console.log(encontrado);
-        navigate("/profile/"+userName);
-      }else{
+        navigate("/profile/" + userName);
+      } else {
         console.log("Error");
-        setELogin("El Correo, el Nombre del Usuario o la Contraseña son Incorrectos. Por favor Intentelo de nuevo.")
+        setELogin(
+          "El Correo, el Nombre del Usuario o la Contraseña son Incorrectos. Por favor Intentelo de nuevo."
+        );
       }
-
-    }else{
+    } else {
       console.log("No ha seleccionado nada");
       setELogin("Error al Iniciar Sesion, por favor Intentelo de nuevo.");
     }
-    
+  };
 
+  async function loginHandler(e) {
+    e.preventDefault();
+    // let res;
+
+    switch (rol) {
+      case "Brigadista":
+        // console.log("rol →", rol);
+        // try {
+        //   res = await axios.post(`${BASE_API_URL}/brigadista/login`, {
+        //     brigName: userName,
+        //     brigMail: email,
+        //     brigPassword: pass,
+        //   });
+        //   let {
+        //     _id,
+        //     brigName,
+        //     brigLastname,
+        //     brigDNI,
+        //     brigMail,
+        //     brigPhone,
+        //     token,
+        //   } = res.data;
+        //   sessionStorage.setItem("loginToken", token);
+        //   sessionStorage.setItem("usuario", {
+        //     _id,
+        //     brigName,
+        //     brigLastname,
+        //     brigDNI,
+        //     brigMail,
+        //     brigPhone,
+        //   });
+        //   navigate(`/profile/${brigName}`);
+        // } catch (err) {
+        //   console.log("err", err);
+        //   console.log("res", res);
+        //   setELogin(
+        //     res?.data?.message
+        //       ? res?.data?.message
+        //       : `Error en el inicio de sesión (${err})`
+        //   );
+        // }
+        await axios
+          .post(`${BASE_API_URL}/brigadista/login`, {
+            brigName: userName,
+            brigMail: email,
+            brigPassword: pass,
+          })
+          .then((res) => {
+            let {
+              _id,
+              brigName,
+              brigLastname,
+              brigDNI,
+              brigMail,
+              brigPhone,
+              token,
+            } = res.data;
+            sessionStorage.setItem("loginToken", token);
+            sessionStorage.setItem("usuario", {
+              _id,
+              brigName,
+              brigLastname,
+              brigDNI,
+              brigMail,
+              brigPhone,
+            });
+            navigate(`/profile/${brigName}`);
+          })
+          .catch((err) => {
+            // console.log("err", err);
+            // console.log("res", res);
+            setELogin(err);
+            // setELogin(
+            //   res?.data?.message
+            //     ? res?.data?.message
+            //     : `Error en el inicio de sesión (${err})`
+            // );
+          });
+        break;
+      case "Administrador":
+        // console.log("rol →", rol);
+        await axios
+          .post(`${BASE_API_URL}/admin/login`, {
+            adminName: userName,
+            adminMail: email,
+            adminPassword: pass,
+          })
+          .then((res) => {
+            let {
+              _id,
+              adminName,
+              adminLastname,
+              adminDNI,
+              adminMail,
+              adminPhone,
+              token,
+            } = res.data;
+            sessionStorage.setItem("loginToken", token);
+            sessionStorage.setItem("usuario", {
+              _id,
+              adminName,
+              adminLastname,
+              adminDNI,
+              adminMail,
+              adminPhone,
+            });
+            navigate("/");
+          })
+          .catch((err) => {
+            setELogin(err);
+            // setELogin(
+            //   res.data?.message
+            //     ? res.data.message
+            //     : "Error desconocido en el inicio de sesión..."
+            // );
+          });
+        break;
+      default:
+        console.log("No se ha seleccionado el rol");
+        break;
+    }
   }
+
   return (
     <div className="App-Login">
-      
-      <Card sx={{width: 300}}>
+      <Card sx={{ width: 300 }}>
         <CardMedia
           component="img"
           height="194"
@@ -123,12 +248,14 @@ function Login() {
           flexDirection: "column",
           gap: 4,
         }}
+        onSubmit={loginHandler}
       >
         <Select
           value={rol}
           onChange={(e) => definirRol(e.target.value)}
           displayEmpty
-          sx={{bgcolor: "white"}}
+          sx={{ bgcolor: "white" }}
+          required
         >
           <MenuItem value="">
             <em>Tipo de Usuario</em>
@@ -137,35 +264,48 @@ function Login() {
           <MenuItem value="Brigadista">Brigadista</MenuItem>
         </Select>
         {campos.map((campo) => (
-          <Box key={campo.id} sx={{width: "100%", position: "relative"}}>
+          <Box key={campo.id} sx={{ width: "100%", position: "relative" }}>
             {createElement(() => iconos[campo.id])}
             <TextField
               id={campo.id}
+              required
               label={campo.label}
               variant="filled"
-              sx={{width: "100%"}}
-
+              sx={{ width: "100%" }}
               //Tomar los valores
-              value={campo.id == "nombre" ? userName: campo.id === "correo" ? email : pass}
-
+              value={
+                campo.id == "nombre"
+                  ? userName
+                  : campo.id === "correo"
+                  ? email
+                  : pass
+              }
               onChange={(e) => {
-                if (campo.id === "nombre"){
+                if (campo.id === "nombre") {
                   setuserName(e.target.value);
-
-                }else if(campo.id === "correo"){
+                } else if (campo.id === "correo") {
                   setEmail(e.target.value);
-                }else if(campo.id === "password"){
+                } else if (campo.id === "password") {
                   setpassword(e.target.value);
                 }
               }}
             />
           </Box>
         ))}
-       
-          <Button variant="contained" onClick={startSession}>Iniciar Sesión</Button>
-          <p style={{color: 'red'}}>{eLogin}</p>
-      
-       
+
+        {/* <Button variant="contained" onClick={startSession}> */}
+        <Button variant="contained" onClick={loginHandler} type="submit">
+          Iniciar Sesión
+        </Button>
+        {eLogin && (
+          <Box style={{ background: "#00000077", padding: "10px" }}>
+            <p style={{ color: "#FF3333", margin: 0 }}>
+              {eLogin?.response?.data?.message
+                ? eLogin.response.data.message
+                : "Error desconocido en el inicio de sesión..."}
+            </p>
+          </Box>
+        )}
       </FormControl>
     </div>
   );
